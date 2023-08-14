aed2ab25ed1c640c0829791369240403c0e6f31c
Ann Bazykina <annzita19@gmail.com>
2023-08-15 00:55:27 +0300

    fix: form

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let burgerMenu = document.querySelector('.burger-menu');
 let nav = document.querySelector('.nav');
-
+let textareaMessage = document.querySelector('.textareafield__input');
+let star = document.querySelector('.star');
 
 burgerMenu.addEventListener('click', () => {
 	burgerMenu.classList.toggle('active');
@@ -11,6 +12,11 @@ burgerMenu.addEventListener('click', () => {
 document.addEventListener('click', (e) => {
 	if (!burgerMenu.contains(e.target)) {
 		burgerMenu.classList.remove('active');
-		mobileMenu.classList.remove('active');
 	}
+});
+
+
+textareaMessage.addEventListener('keyup', () => {
+	if(textareaMessage.value.length > 0) star.style.display = 'none';
+	if(textareaMessage.value.length === 0) star.style.display = 'block';
 });
